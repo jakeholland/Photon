a388576e9fed365c7e6ec2210e77a1bde9cfac83
Jake Holland <jholland@jimmyjohns.com>
2019-01-04 15:16:05 -0600

    Preliminary Icons

--- GifCapture/AppDelegate.swift
+++ GifCapture/AppDelegate.swift
@@ -5,7 +5,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     
     private let recordingManager = RecordingManager.shared
     private let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-    private let recordMenuItem = NSMenuItem(title: "Record", action: #selector(toggleRecording(_:)), keyEquivalent: "")
+    private let recordMenuItem = NSMenuItem(title: "Start Recording", action: #selector(toggleRecording(_:)), keyEquivalent: "")
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         buildMenu()
@@ -27,12 +27,11 @@ extension AppDelegate {
         guard let button = statusItem.button else { fatalError("statusItem not found") }
 
         button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
-        button.contentTintColor = .white
         
         let menu = NSMenu()
         menu.addItem(recordMenuItem)
         menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
         
         statusItem.menu = menu
     }
