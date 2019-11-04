8055e5647fe9d570104694901807a678f1aebf3f
Jake Holland <jholland@jimmyjohns.com>
2019-11-04 09:55:05 -0600

    Fix parsing issue

--- Photon/Helpers/SimulatorDevice.swift
+++ Photon/Helpers/SimulatorDevice.swift
@@ -3,10 +3,9 @@ struct SimulatorDevices: Decodable {
 }
 
 struct SimulatorDevice: Decodable {
-    let availability: String
     let state: String
     let isAvailable: Bool
     let name: String
     let udid: String
-    let availabilityError: String
+    let availabilityError: String?
 }
